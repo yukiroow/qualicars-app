b2f318aeab4911439755366e168efea7f93fcb71
Loanne Malou Abu <2213118@slu.edu.ph>
2026-01-30 00:03:17 +0800

    added parameterized query for login

--- backend/src/main/java/com/qualinterns/dealership_app_api/repo/AgentRepo.java
+++ backend/src/main/java/com/qualinterns/dealership_app_api/repo/AgentRepo.java
@@ -2,6 +2,8 @@ package com.qualinterns.dealership_app_api.repo;
 
 import com.qualinterns.dealership_app_api.model.Agent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
@@ -9,4 +11,8 @@ import java.util.Optional;
 @RepositoryRestResource
 public interface AgentRepo extends JpaRepository<Agent, Short> {
     Optional<Agent> findByUsername(String username);
+
+    // Parameterized query
+    @Query("SELECT a FROM agent WHERE a.username = :username")
+    Optional<Agent> findByUsernameParameterized(@Param("username") String username);
 }
