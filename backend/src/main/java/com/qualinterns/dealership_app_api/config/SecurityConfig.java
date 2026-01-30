71e4e4de04ecf8d93c16a19ffc8b95d902cb42ca
yukiroow <hcdominguez07@gmail.com>
2026-01-30 14:44:35 +0800

    Add proper jwt auth

--- backend/src/main/java/com/qualinterns/dealership_app_api/config/SecurityConfig.java
+++ backend/src/main/java/com/qualinterns/dealership_app_api/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.qualinterns.dealership_app_api.config;
 
 import com.qualinterns.dealership_app_api.security.JwtAuthenticationFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,25 +21,29 @@ public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) {
         http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/agents/login").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().denyAll()
                 )
                 .logout(logout -> logout
                         .logoutUrl("/api/agents/logout")
                         .deleteCookies("jwtToken")
-                        .logoutSuccessUrl("/api/agents/login")
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            response.setStatus(HttpServletResponse.SC_OK);
+                            response.getWriter().write("{\"message\": \"Logout successful\"}");
+                        })
                 )
                 .exceptionHandling(handling -> handling
                         .authenticationEntryPoint((request, response, authException) -> {
                             response.setStatus(401);
                             response.setContentType("application/json");
-                            response.getWriter().write("{\"error\":\"Unauthorized\"}");
+                            response.getWriter().write("{\"error\":\"Unauthorized\", \"exception\":" + authException.getMessage() + "}");
                         })
                 );
 
